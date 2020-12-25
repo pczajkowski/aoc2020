b45df5cd2a91a65228d5b8b408ef779e0285cdc5
Piotr Czajkowski <pczajkowski@gmail.com>
2020-12-25 09:03:13 +0100

    Have size of loops

--- day25/day25.go
+++ day25/day25.go
@@ -32,6 +32,20 @@ func readFile(file *os.File) []int {
 	return keys
 }
 
+func establishLoopSize(key int) int {
+	iterations := 0
+	value := 1
+
+	for value != key {
+		value *= 7
+		value %= 20201227
+
+		iterations++
+	}
+
+	return iterations
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("You need to specify a file!")
@@ -49,5 +63,8 @@ func main() {
 		log.Fatalf("Failed to close file: %s", err)
 	}
 
-	fmt.Println(keys)
+	key1LoopSize := establishLoopSize(keys[0])
+	key2LoopSize := establishLoopSize(keys[1])
+
+	fmt.Println(key1LoopSize, key2LoopSize)
 }
